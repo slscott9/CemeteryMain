bd7919dece7bc2052bd0f00e7f54ebbd285d6e8d
slscott9 <59343900+slscott9@users.noreply.github.com>
2020-07-27 09:39:56 -0500

    added binding adapters to set fragment_list_item's views. Recyceler view adapter sets the binding object's data. Binding adapters take care of setting views

--- app/src/main/java/hfad/com/cemeteryapp1/adapters/CemeteryListAdapter.kt
+++ app/src/main/java/hfad/com/cemeteryapp1/adapters/CemeteryListAdapter.kt
@@ -1,55 +1,68 @@
 package hfad.com.cemeteryapp1.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.databinding.DataBindingUtil.inflate
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import hfad.com.cemeteryapp1.R
 import hfad.com.cemeteryapp1.database.Cemetery
+import hfad.com.cemeteryapp1.databinding.CemeteryListItemBinding
 
+/*
+        1. We use data binding in the list_item xml it has a reference to our database
+        2. we need to make the adapter log data to the layout using data binding now
 
-class CemeteryListAdapter: ListAdapter<Cemetery, CemeteryListAdapter.ViewHolder>(CemeteryDiffUtilCallback()){
+        One pattern to do this is to is to update our ViewHolder to hold binding objects. The big idea about data binding is to create an object that
+        connects, maps, and binds two peices of information together at compile time, so that you dont have to look for it at run time.
+        The object that surfaces these bindings to you is the binding object (camel case of layout name with Binding at end). Its created by the compiler.
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position) //provided by ListAdater class
-        holder.name.text = item.cemeteryName
-        holder.location.text = item.cemeteryLocation
-//        holder.state.text = item.cemeteryState
-//        holder.name.text = item.cemeteryCounty
-//        holder.name.text = item.township
-//        holder.name.text = item.range
-//        holder.name.text = item.section
-//        holder.name.text = item.spot
-//        holder.firstYear.text =
-    }
+        Recycler view doesnt know anything about binding objects or data binding. This is okay because we can stil expose recyelr view adapter api.
+        This is part of the power of recycler view adapter's api. It lets us design our code in a way that makes sense to us as long as we meet the requirements of the adapter
+        interface.
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        May have to rebuild project to generate the binding object
+
+        3. now that we have the binding object we can use it to inflate the layout for us. Replace val view = layoutInflater() with binding object
+
+        4. alt enter on view parameter change to item view
+
+        5. make binding a property in onCreateViewHolder
+
+        9. we go into list_item xml and use app:setCemeteryName="@{@cemetery}"
+            -setCemeteryName is the annotation name we gave our binding adapter in Binding.Util. cemetery is the binding object variable name in the xml
+            - it is populated with data from our Bind.Util adapters our views will be automatically updated through the binding adapters
+         */
+
+class CemeteryListAdapter: ListAdapter<Cemetery, CemeteryListAdapter.ViewHolder>(CemeteryDiffUtilCallback()){ //1.
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder { //2. inflate our list item layout the recycler view will use for each row
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-            .inflate(R.layout.cemetery_list_item, parent, false)
+        val binding = CemeteryListItemBinding.inflate(layoutInflater, parent, false) //3. use binding to inflate the list_item layout
 
-        return ViewHolder(view)
+        return ViewHolder(binding) //4. alt enter on view parameter change to item view
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val name: TextView = itemView.findViewById(R.id.cemNameList)
-        val location: TextView = itemView.findViewById(R.id.cemLocationList)
-        //val state: TextView = itemView.findViewById(R.id.cemStateTv)
-//        val county: ImageView = itemView.findViewById(R.id.cemCountyTv)
-//        val township: ImageView = itemView.findViewById(R.id.cemTownShipTv)
-//        val range: ImageView = itemView.findViewById(R.id.cemRangeTv)
-//        val section: ImageView = itemView.findViewById(R.id.cemSectionTv)
-//        val spot: ImageView = itemView.findViewById(R.id.cemSpotTv)
-//        val gps: ImageView = itemView.findViewById(R.id.cemGPSTv)
-//        val firstYear: ImageView = itemView.findViewById(R.id.cemFirstYearTv)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) { //6. set our ViewGroups views
+        val item = getItem(position) //provided by ListAdater class, we now have the object at the position in our list. We can use it to access its properties and set our views with it.
+//        holder.binding.cemNameList.text = item.cemeteryName
+//        holder.binding.cemLocationList.text = item.cemeteryLocation
+
+        holder.bind(item) //9. call the method that binds data to our binding object in list_item xml.
+    }
+
+    class ViewHolder (val binding: CemeteryListItemBinding) : RecyclerView.ViewHolder(binding.root){ //7. make binding a val property and pass RecyclerView.ViewHolder the root view (<layout> in list item xml replaced our root view)
+
+        //binding.root is the <layout> tag from list_item xml
+
+        fun bind(item: Cemetery){   //8. gives our cemetery <data> binding tag data to bind
+            binding.cemetery = item
+            binding.executePendingBindings()
+        }
     }
 }
+
+//diff util calulates the minimum changes needed it is more efficient. Only updates the items that are changed.
+//create diff util class that handles changes for us
 
 class CemeteryDiffUtilCallback: DiffUtil.ItemCallback<Cemetery>(){
     override fun areItemsTheSame(oldItem: Cemetery, newItem: Cemetery): Boolean {
---
+++ app/src/main/java/hfad/com/cemeteryapp1/utils/BindingUtils.kt
@@ -0,0 +1,34 @@
+package hfad.com.cemeteryapp1.utils
+
+import android.widget.TextView
+import androidx.databinding.Bindable
+import androidx.databinding.BindingAdapter
+import hfad.com.cemeteryapp1.database.Cemetery
+
+/*
+    We used binding to set our views in the adapter's ViewHolder, so far the code just uses the binding object to avoid calling findViewById
+    We can update this to use data binding to set our views for us. To do this we need to make binding adapters.
+    Binding adapters are just like recycler view adapters. Its an adapter that adapts our application data into something data binding can display
+    on the screen
+
+    To make a binding adapter you make a method that takes a view and some data. The binding adapter is responsible for updating the view to represent the data
+    we will need two one for the name and one for the location. We can write the adapter as an extention function
+
+    IMPORTANT - make sure to use the Cemetery @Entity class as the list_item xml variable
+
+    1. we have to bind the data to the views that is what makes it a binding adapter, the method adapts our data to something the binding can use
+ */
+
+@BindingAdapter("setCemeteryName")
+fun TextView.setCemeteryName(item: Cemetery?){
+    item?.let {
+        text = item.cemeteryName
+    }
+}
+
+@BindingAdapter("setCemeteryLocation")
+fun TextView.setCemeteryLocation(item: Cemetery?){
+    item?.let {
+        text = item.cemeteryLocation
+    }
+}
